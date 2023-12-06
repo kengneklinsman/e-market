4c4734dbd0304ae154d3ed9e7a1f11a78496220f
Bushaija <bushaijarobert@gmail.com>
2023-12-06 16:48:22 +0200

    implement the domain classes

---
+++ Models/ViewModels/ClientLoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Emarketing.ViewModels
+{
+    public class ClientLoginViewModel
+    {
+        [Required(ErrorMessage = "Username or Email is required.")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "username or email is required.")]
+        public string email { get; set; }
+    }
+}
---
+++ Models/ViewModels/ClientSignUpViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Emarketing.ViewModels
+{
+      public class ClientSignUpViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required.")]
+        public int Phone { get; set; }
+
+        
+    }
+
+}
